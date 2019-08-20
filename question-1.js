b7f8d43471c14507dbabd12c05ad051b34d6648a
teddypiter <53746400+teddypiter@users.noreply.github.com>
2019-08-20 22:32:08 +0700

    Update question-1.js

--- question-1.js
+++ question-1.js
@@ -1,29 +1,32 @@
 function solution(record) {
     var answer = [];
     let command = [];
-    let user = {};
-
+    let user = {}; //hash table to save user id and username
+    
+    //get latest username from specific id
     for(let i = 0; i < record.length; i++){
-      command.push(record[i].split(' '));
+      command.push(record[i].split(' ')); //make value of command easier to get 
       switch(command[i][0]){
         case 'Enter':
-          user[command[i][1]] = command[i][2];
+          user[command[i][1]] = command[i][2];//assign username
           break;
         case 'Leave':
+          //leave command doesn't change username    
           break;
         case 'Change':
-          user[command[i][1]] = command[i][2];
+          user[command[i][1]] = command[i][2];//assign username
           break;  
       }
     }
-
+    
+    //make sentences from command and stored user data
     for(let i = 0; i < command.length; i++){
       switch(command[i][0]){
         case 'Enter':
           answer.push(user[command[i][1]] +' came in.');
           break;
         case 'Leave':
-          answer.push(user[command[i][1]] +' has left');
+          answer.push(user[command[i][1]] +' has left.');
           break; 
       }
     }
