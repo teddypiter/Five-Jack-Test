35db7b0eee76fbdf402d662b3f8173d3ab88ca36
teddypiter <53746400+teddypiter@users.noreply.github.com>
2019-08-20 22:40:34 +0700

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -1,13 +1,17 @@
 function solution(N, users) {
     var answer = [];
     let stageRate = [];
+    
+    //get failure rate for every stage
     for(let i = 1; i <= N; i++){
       let stage = {
         number: 0,
         rate: 0
-      }
+      } 
       let numerator = 0;
       let denominator = 0;
+        
+      //calculate failure rate  
       for(let j = 0; j < users.length; j++){
         stage.number = i;
         if(i == users[j]){
@@ -18,10 +22,14 @@ function solution(N, users) {
         }
       }
       stage.rate = numerator/denominator;
+        
+      //save stage object to array for sorting 
       stageRate.push(stage);
     }
-    stageRate.sort((a,b)=>b.rate-a.rate);
-    stageRate.forEach(a=>answer.push(a.number));
+    //sort the stage from biggest to lowest failure rate 
+    stageRate.sort((a,b) => b.rate - a.rate);
+    stageRate.forEach(a => answer.push(a.number));
+    
     console.log(answer);
     return answer;
 }
