e173d1ce8851cbbfc1ef3e930ef00db29c245927
teddypiter <53746400+teddypiter@users.noreply.github.com>
2019-08-21 23:00:44 +0700

    Update question-3.js

--- question-3.js
+++ question-3.js
@@ -36,16 +36,9 @@ function solution(relation) {
         answer++;
       }
     }
-    console.log(answer);
+    
     return answer;
 }
 
-
-solution([['100','ryan','music','2'],
-          ['200','apeach','math','2'],
-          ['300','tube','computer','3'],
-          ['400','con','computer','4'],
-          ['500','muzi','music','3'],
-          ['600','apeach','music','2']]);
 
           
